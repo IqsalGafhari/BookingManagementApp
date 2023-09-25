4f89a0e7fd035aa2ccf5fa864793c47f35169ff3
J3C218193 <72546948+J3C218193@users.noreply.github.com>
2023-09-25 21:19:31 +0700

    Employee.cs

--- BookingManagementApp-main(3)/BookingManagementApp-main/API/Models/Employee.cs
+++ BookingManagementApp-main(3)/BookingManagementApp-main/API/Models/Employee.cs
@@ -1,5 +1,4 @@
-﻿using API.Utilities.Enum;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
